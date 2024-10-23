a06728b7a745254674115e08a5197855b5a46e4a
Yonas Negese M <yonasnegese250@gmail.com>
2024-10-23 11:33:23 +0300

    Start abyssinia brochure frontend

---
+++ Client/abyssinia_frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <div>ABYSSINIA TUTORS AND TRAINING CENTER</div>
+    </>
+  )
+}
+
+export default App
---
+++ Client/abyssinia_frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './header.module.css'
+function Header() {
+  return (
+    <>
+      <div className={styles.header_container}>
+            <p>ABYSSINIA</p>
+      </div>
+    </>
+  )
+}
+
+export default Header
