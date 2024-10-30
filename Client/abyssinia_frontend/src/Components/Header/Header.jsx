aae01adc0e3a4f8d948a7cba362ac51b012cb472
Yonas Negese M <yonasnegese250@gmail.com>
2024-10-30 23:29:23 +0300

    Update header

--- Client/abyssinia_frontend/src/Components/Header/Header.jsx
+++ Client/abyssinia_frontend/src/Components/Header/Header.jsx
@@ -3,7 +3,17 @@ function Header() {
   return (
     <>
       <div className={styles.header_container}>
-            <p>ABYSSINIA</p>
+          <div className={styles.abyssinia_logo}>
+              <img src="" alt="" />
+              <p>ABYSSINIA</p>
+          </div>
+          <div className={styles.header_links}>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact</li>
+              </ul>
+          </div>
       </div>
     </>
   )
